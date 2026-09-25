fd11d4ea1dfa92d86f9f0f3885c819dfbcf46630
Daria <daria.osokina@levelaccess.com>
2026-09-25 11:33:57 +0300

    initial commit

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,14 +1,9 @@
 import type { Config } from "@level-ci/cli";
 
 export default {
- organization: "daria-osokina-1-levelaccess-com-kofcj",
- project: "level-ci-playwright-sample-github",
+ organization: 'daria-osokina-1-levelaccess-com-kofcj',
+ project: 'test-website-1-mirko-github-daria',
  token: process.env.LEVEL_CI_TOKEN,
  server: "https://api.dev.userway.dev",
  reportPaths: ['./level-ci-reports']
- override: {
- 'main': {
-  scope: 'delta'
-  },
- }
 } satisfies Config;
